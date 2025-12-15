22a23873287163753ab224da72e8901ae61235a1
floriansalvi <floriansalvipro@gmail.com>
2025-12-15 11:42:04 +0100

    initial branch commit

--- components/The/TheHeroSection.vue
+++ components/The/TheHeroSection.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, computed } from 'vue';
 const props = defineProps({
     elements: {
         type: Array,
@@ -9,6 +10,18 @@ const props = defineProps({
             )
     }
 })
+
+const currentIndex = ref(0);
+const currentElement = computed(() => props.elements[currentIndex.value]);
+
+const prev = () => {
+    currentIndex.value = (currentIndex.value - 1 + props.elements.length) % props.elements.length;
+}
+
+const next = () => {
+    currentIndex.value P 
+}
+
 </script>
 
 <template>
