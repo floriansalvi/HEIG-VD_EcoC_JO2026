08762598efad4bf35cdf9c111ebf91892dd54010
anna142204 <anna142204@gmail.com>
2026-01-16 09:28:24 +0100

    BaseArticleCard and ArticlesView visual

--- components/Base/BaseArticleCard.vue
+++ components/Base/BaseArticleCard.vue
@@ -24,16 +24,22 @@ const props = defineProps({
 </script>
 
 <template>
-    <article class="">
-        <NuxtLink :to="link" class="" :aria-label="`Lire l'article ${title}`">
+    <article class="flex flex-col items-start w-[397px]">
+        <NuxtLink :to="link" class="w-full" :aria-label="`Lire l'article ${title}`">
             <BaseImg
                 :src="img.src"
                 :alt="img.alt"
-                wrapperClass=""
-                imgClass=""
+                wrapperClass="h-[250px] w-full rounded-[15px] overflow-hidden"
+                imgClass="w-full h-full object-cover"
             />
-            <span v-if="category" class="">{{ category }}</span>
-            <h3 class="">{{ title }}</h3>
+            <div class="flex flex-col gap-[4px] items-start justify-center pt-[10px] pb-[10px] pr-[10px] w-full">
+                <span v-if="category" class="inline-flex items-center justify-center px-[5px] bg-[var(--color-light-green)] rounded-[50px] text-[var(--color-blue)] text-[14px] font-semibold">
+                    {{ category }}
+                </span>
+                <h3 class="text-[20px] font-semibold text-black break-words w-full">
+                    {{ title }}
+                </h3>
+            </div>
         </NuxtLink>
     </article>
 </template>
--- views/ArticlesView.vue
+++ views/ArticlesView.vue
@@ -4,15 +4,18 @@ import BaseArticleCard from '../components/Base/BaseArticleCard.vue';
 </script>
 
 <template>
-  <section>
-    <h2>Dernières actualités</h2>
+  <section class="w-full max-w-[1362px] mx-auto px-5 pb-10">
+     <h2 class="font-[Poppins] font-bold text-[28px] md:text-[32px] text-[var(--color-dark-blue)]">
+      Dernières actualités
+    </h2>
+    <div class="flex flex-row gap-6 py-4">
     <BaseArticleCard
         v-for="(article, index) in articles"
         :title="article.title"
         :link="article.link"
         :category="article?.category"
         :img="article.img"
-    />
+    /></div>
   </section>
 </template>
 
